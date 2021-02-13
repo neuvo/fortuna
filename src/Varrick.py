18e0c6c3c58662d4029c39056882e6b402ffe2de
James Hightower <jrhighto@usc.edu>
2021-02-12 23:34:32 -0800

    Users can now change their own nicknames on the fly

--- src/Varrick.py
+++ src/Varrick.py
@@ -1,5 +1,15 @@
+import os
+
+
+def parse_nick(*args):
+    if len(args) != 1:
+        return "Missing nickname"
+    else:
+        return ''
+
+
 class Varrick:
-    def __init__(self,dbpath):
+    def __init__(self, dbpath):
         self.dbpath = dbpath
         self.nicknames = {}
         fo = open(dbpath)
@@ -8,15 +18,30 @@ class Varrick:
             pair = line.split(' ')
 
             if len(pair) == 2:
-                self.giveNick(pair[0],pair[1])
+                self.give_nick(pair[0], pair[1])
 
-    def getNick(self,name):
+        fo.close()
+
+    def get_nick(self, name) -> str:
         if name in self.nicknames:
             return self.nicknames[name]
         else:
             return name
 
-    def giveNick(self,name,nick):
+    def update_nick(self, ctx, *args) -> str:
+        parse_output = parse_nick(args)
+
+        if len(parse_output) > 0:
+            return parse_output
+
+        user = str(ctx.message.author)
+        give_nick_output = self.give_nick(user, args[0])
+        if len(give_nick_output) > 0:
+            return give_nick_output
+        self.save_nicknames()
+        return user + ' updated nickname to ' + self.nicknames[user]
+
+    def give_nick(self, name, nick) -> str:
         if len(name) == 0 or len(nick) == 0:
             return 'Nicknames cannot be empty'
         else:
@@ -26,10 +51,20 @@ class Varrick:
 
         # pair is valid
         self.nicknames[name] = nick
+        return ''
 
-    def removeNick(self,name):
+    def remove_nick(self, name) -> str:
         if name in self.nicknames:
             self.nicknames.pop(name)
             return name + ' is no longer nicknamed'
         else:
             return name + ' already has no nickname'
+
+    def save_nicknames(self):
+        fo = open(self.dbpath+'.tmp','w+')
+        for name in self.nicknames:
+            fo.write(name + ' ' + self.nicknames[name]+'\n')
+
+        fo.close()
+
+        os.replace(self.dbpath+'.tmp', self.dbpath)
--- src/fortuna.py
+++ src/fortuna.py
@@ -15,33 +15,39 @@ from Varrick import Varrick
 
 
 def nCr(n, r):
-    r = min(r, n-r)
-    numer = reduce(op.mul, range(n, n-r, -1), 1)
-    denom = reduce(op.mul, range(1, r+1), 1)
-    return numer//denom
+    r = min(r, n - r)
+    numer = reduce(op.mul, range(n, n - r, -1), 1)
+    denom = reduce(op.mul, range(1, r + 1), 1)
+    return numer // denom
+
 
 TOKEN = open('../data/token.cfg').readline()
 
-client = Bot(description="Fortuna: a dice assistant by neuvo#1301", command_prefix="/", pm_help = True)
+client = Bot(description="Fortuna: a dice assistant by neuvo#1301", command_prefix="/", pm_help=True)
 
 USAGE_MSG = "Usage: %sroll <num dice> <difficulty>" % client.command_prefix
 
 USAGE_MSG_SR = "Usage: %sroll <num dice>" % client.command_prefix
 
-USAGE_MSG_SW = ('Usage: %sroll <die1> <optional:die2> <optional:mod>\nExample: %sroll 1d8 1d6 +2\nNote: mod must have a + or a - in front. Dice and mods may be in any order.\n' % (client.command_prefix,client.command_prefix))
+USAGE_MSG_SW = (
+            'Usage: %sroll <die1> <optional:die2> <optional:mod>\nExample: %sroll 1d8 1d6 +2\nNote: mod must have a + or a - in front. Dice and mods may be in any order.\n' % (
+    client.command_prefix, client.command_prefix))
 
 # to be used only for testing. Users shouldn't see this message.
 ERROR_MSG = "Incorrectly formatted input\n"
 
-class Game(Enum):
-    mageTheAscension=1
-    strikeLegion=2
-    pathfinder=3
-    shadowrun=4
-    savageWorlds=5
 
-GAMES = { Game.mageTheAscension:"Mage: The Ascension", Game.strikeLegion:"Strike Legion", Game.pathfinder:"Pathfinder",
-    Game.shadowrun:"Shadowrun", Game.savageWorlds:"Savage Worlds" }
+class Game(Enum):
+    mageTheAscension = 1
+    strikeLegion = 2
+    pathfinder = 3
+    shadowrun = 4
+    savageWorlds = 5
+
+
+GAMES = {Game.mageTheAscension: "Mage: The Ascension", Game.strikeLegion: "Strike Legion",
+         Game.pathfinder: "Pathfinder",
+         Game.shadowrun: "Shadowrun", Game.savageWorlds: "Savage Worlds"}
 
 DEFAULT_GAME = Game.savageWorlds
 
@@ -52,6 +58,7 @@ savageWorlds = SavageWorlds.Game(client.command_prefix)
 fortuneCookie = FortuneCookie()
 
 varrick = Varrick('../data/nicknames.db')
+
 
 # rolls a set of dice with the given number of sides
 # adds all the given modifiers
@@ -67,19 +74,20 @@ def roll_dice_sum(num, sides, mods):
     # perform all the rolls, tally the total
     for i in range(num):
         result = random.randint(1, sides)
-        outstring = "Roll %d: %d" % (i+1, result)
+        outstring = "Roll %d: %d" % (i + 1, result)
         result_string += outstring + '\n'
         total += result
 
     # add the modifiers to the total
     # for i in mods:
-        # total += mods[i]
+    # total += mods[i]
 
     total += mods
 
     result_string += "Total: %d\n" % total
 
     return result_string
+
 
 # rolls a set of dice versus a given difficulty
 # results matching or exceeding the difficulty succeed
@@ -89,7 +97,6 @@ def roll_dice_sum(num, sides, mods):
 # based on Mage: The Ascension rolling rules
 # works just as well for Strike Legion
 def roll_dice_diff(num, sides, diff):
-
     # stores the complete result string, to be posted in the chat
     result_string = ""
 
@@ -103,7 +110,7 @@ def roll_dice_diff(num, sides, diff):
     # use + to indicate success, - to indicate failure (natural 1)
     for i in range(num):
         result = random.randint(1, sides)
-        outstring = "Roll %d: %d" % (i+1, result)
+        outstring = "Roll %d: %d" % (i + 1, result)
         if result == 1:
             succ -= 1
             outstring += "-"
@@ -127,6 +134,7 @@ def roll_dice_diff(num, sides, diff):
 
     return result_string
 
+
 # Performs tests in the D6 system. Intended for use with Shadowrun 5e.
 # Num: the number of dice to roll.
 def roll_dice_d6(num):
@@ -143,7 +151,7 @@ def roll_dice_d6(num):
     # use + to indicate hits, - to indicate ones
     for i in range(num):
         result = random.randint(1, 6)
-        outstring = "Roll %d: %d" % (i+1, result)
+        outstring = "Roll %d: %d" % (i + 1, result)
         if result == 1:
             ones += 1
             outstring += "-"
@@ -160,17 +168,18 @@ def roll_dice_d6(num):
     glitch_string = "%d one" % ones
     if ones != 1:
         glitch_string += "s"
-    
+
     result_string += hits_string + '\n'
     result_string += glitch_string + '\n'
 
     return result_string
 
+
 def roll_dreidel(num):
     # stores the complete result string, to be posted in the chat
     results = []
-    mapping = {0:"../assets/nun.png", 1:"../assets/gimel.png", 2:"../assets/hay.png", 3:"../assets/shin.png"}
-    result = random.randint(0,3)
+    mapping = {0: "../assets/nun.png", 1: "../assets/gimel.png", 2: "../assets/hay.png", 3: "../assets/shin.png"}
+    result = random.randint(0, 3)
 
     # perform all the rolls
     # use + to indicate hits, - to indicate ones
@@ -179,15 +188,19 @@ def roll_dreidel(num):
 
     return results
 
+
 def get_name(ctx):
-    return str(varrick.getNick(str(ctx.message.author)))
+    return varrick.get_nick(str(ctx.message.author))
+
 
 # Prints basic information to console and sets presence on Discord
 @client.event
 async def on_ready():
-    print('Logged in as '+client.user.name+' (ID:'+str(client.user.id)+') \nConnected to '+str(len(set(client.get_all_members())))+' users')
+    print('Logged in as ' + client.user.name + ' (ID:' + str(client.user.id) + ') \nConnected to ' + str(
+        len(set(client.get_all_members()))) + ' users')
     print('--------')
-    print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
+    print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__,
+                                                                               platform.python_version()))
     print('--------')
     print('Use this link to invite {}:'.format(client.user.name))
     print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
@@ -195,6 +208,7 @@ async def on_ready():
     print('You are running ' + client.user.name + 'v2.2')
     print('Created by neuvo#1301')
     return await client.change_presence(activity=discord.Game(name=GAMES[DEFAULT_GAME]))
+
 
 # Roll command: rolls a specified number and type of dice
 # Exact behavior depends on game mode
@@ -211,10 +225,10 @@ async def roll(ctx, *args):
 
         results = roll_dreidel(num);
         for result in results:
-            await ctx.send(str(varrick.getNick(str(ctx.message.author))) + ' rolls:')
+            await ctx.send(str(varrick.get_nick(str(ctx.message.author))) + ' rolls:')
             await ctx.send(file=discord.File(result))
     elif current_game == Game.savageWorlds:
-        await ctx.send(savageWorlds.roll(str(varrick.getNick(str(ctx.message.author))), args))
+        await ctx.send(savageWorlds.roll(str(varrick.get_nick(str(ctx.message.author))), args))
     elif current_game != Game.shadowrun:
         if len(args) != 2:
             await ctx.send(USAGE_MSG)
@@ -229,24 +243,26 @@ async def roll(ctx, *args):
         # if the user did not specify number of dice, assume 1
         if d_index == 0:
             num = 1
-            sides = int(nds[d_index+1:])
+            sides = int(nds[d_index + 1:])
         # if the user did not specify the number of sides but still typed 'd', assume sides=10
-        elif d_index == len(nds)-1:
+        elif d_index == len(nds) - 1:
             await ctx.send("Number of sides unspecified, assuming d10")
             num = int(nds[:d_index])
         # if the user specified the number of sides, then we adapt to that
         elif d_index != -1 and d_index > 0:
             num = int(nds[:d_index])
-            sides = int(nds[d_index+1:])
+            sides = int(nds[d_index + 1:])
         # default: user did not specify number of sides, assume 10
         elif d_index == -1:
             num = int(nds)
 
         if current_game == Game.mageTheAscension or current_game == Game.strikeLegion:
-            await ctx.send("%s rolls %dd%d, difficulty %d" % (varrick.getNick(str(ctx.message.author)), num, sides, diff))
+            await ctx.send(
+                "%s rolls %dd%d, difficulty %d" % (varrick.get_nick(str(ctx.message.author)), num, sides, diff))
             await ctx.send(roll_dice_diff(num, sides, diff))
         elif current_game == Game.pathfinder:
-            await ctx.send("%s rolls %dd%d, with %d bonus" % (varrick.getNick(str(ctx.message.author)), num, sides, diff))
+            await ctx.send(
+                "%s rolls %dd%d, with %d bonus" % (varrick.get_nick(str(ctx.message.author)), num, sides, diff))
             await ctx.send(roll_dice_sum(num, sides, diff))
     else:
         if len(args) != 1:
@@ -255,27 +271,29 @@ async def roll(ctx, *args):
 
         num = int(args[0])
 
-        await ctx.send("%s rolls %dd6" % (varrick.getNick(str(ctx.message.author)), num))
+        await ctx.send("%s rolls %dd6" % (varrick.get_nick(str(ctx.message.author)), num))
         await ctx.send(roll_dice_d6(num))
+
 
 @commands.command(pass_context=True)
 async def r(ctx, *args):
-    await roll(ctx,*args)
+    await roll(ctx, *args)
+
 
 @commands.command(pass_context=True)
 async def analyze(ctx, *args):
     if current_game == Game.shadowrun:
         outstring = ""
-        pwin = 1.0/3.0
-        ploss = 2.0/3.0
+        pwin = 1.0 / 3.0
+        ploss = 2.0 / 3.0
 
         if len(args) == 1:
             nd6 = int(args[0])
-            for thresh in range(1, nd6+1):
+            for thresh in range(1, nd6 + 1):
                 p = 0.0
 
-                for i in range(thresh, nd6+1):
-                    p += nCr(nd6, i) * pow(pwin, i) * pow(ploss, nd6-i)
+                for i in range(thresh, nd6 + 1):
+                    p += nCr(nd6, i) * pow(pwin, i) * pow(ploss, nd6 - i)
 
                 if (p < 0.000001):
                     break
@@ -289,18 +307,19 @@ async def analyze(ctx, *args):
 
             p = 0.0
 
-            for i in range(thresh, nd6+1):
-                p += nCr(nd6, i) * pow(pwin, i) * pow(ploss, nd6-i)
+            for i in range(thresh, nd6 + 1):
+                p += nCr(nd6, i) * pow(pwin, i) * pow(ploss, nd6 - i)
 
             await ctx.send("P(%d+ successes with %d rolls) = %f" % (thresh, nd6, p))
         else:
             await ctx.send(USAGE_MSG_SR)
             return
 
+
 # Updates the current game
 # This affects dice rolling behavior
 @commands.command(pass_context=True)
-async def setgame(ctx, *args):
+async def set_game(ctx, *args):
     global current_game
     # combine all input args to form a game name
     game_name = ""
@@ -311,7 +330,7 @@ async def setgame(ctx, *args):
     game_name = game_name[0:-1]
 
     # check if the game name is a string
-    if not isinstance(game_name,str):
+    if not isinstance(game_name, str):
         await ctx.send(str(game_name) + " isn't a string! How'd you manage that with a keyboard?\n")
         return
 
@@ -324,7 +343,8 @@ async def setgame(ctx, *args):
             if GAMES[key] == game_name:
                 current_game = key
                 await ctx.send("Now playing " + game_name + "\n")
-                return await client.change_presence(status=Status.online, activity=discord.Game(name=GAMES[current_game]))
+                return await client.change_presence(status=Status.online,
+                                                    activity=discord.Game(name=GAMES[current_game]))
     else:
         await ctx.send("I don't know how to play " + game_name + "!\n")
         help_string = "I only know how to play: "
@@ -334,13 +354,15 @@ async def setgame(ctx, *args):
 
         await ctx.send(help_string[0:-2] + "\n")
 
+
 @commands.command(pass_context=True)
 async def draw(ctx, *args):
     if current_game != Game.savageWorlds:
         await ctx.send('draw not supported in current game system.')
         return
 
-    await ctx.send(savageWorlds.draw(get_name(ctx),args))
+    await ctx.send(savageWorlds.draw(get_name(ctx), args))
+
 
 @commands.command(pass_context=True)
 async def shuffle(ctx, *args):
@@ -348,6 +370,7 @@ async def shuffle(ctx, *args):
         await ctx.send(savageWorlds.shuffle(str(ctx.message.author)))
     else:
         await ctx.send('hand not supported in current game system.')
+
 
 @commands.command(pass_context=True)
 async def hand(ctx, *args):
@@ -358,14 +381,16 @@ async def hand(ctx, *args):
     if len(args) == 1 and len(args[0]) > 0:
         await ctx.send(savageWorlds.hand(str(args[0])))
     else:
-        await ctx.send(savageWorlds.hand(str(varrick.getNick(str(ctx.message.author)))))
+        await ctx.send(savageWorlds.hand(str(varrick.get_nick(str(ctx.message.author)))))
+
 
 @commands.command(pass_context=True)
 async def discard(ctx, *args):
     if not current_game == Game.savageWorlds:
         await ctx.send('draw not supported in current game system.')
     else:
-        await ctx.send(savageWorlds.discard(get_name(ctx),args))
+        await ctx.send(savageWorlds.discard(get_name(ctx), args))
+
 
 @commands.command(pass_context=True)
 async def countdown(ctx, *args):
@@ -374,9 +399,11 @@ async def countdown(ctx, *args):
     else:
         await ctx.send('countdown not supported in current game system.')
 
+
 @commands.command(pass_context=True)
 async def cd(ctx, *args):
-    await countdown(ctx,*args)
+    await countdown(ctx, *args)
+
 
 @commands.command(pass_context=True)
 async def cardsleft(ctx):
@@ -385,10 +412,12 @@ async def cardsleft(ctx):
     else:
         await ctx.send('cardsleft not supported in current game system.')
 
+
 @commands.command(pass_context=True)
 async def trade(ctx, *args):
     if current_game == Game.savageWorlds:
         await ctx.send()
+
 
 @commands.command(pass_context=True)
 async def cookie(ctx, *args):
@@ -401,31 +430,41 @@ async def cookie(ctx, *args):
             await ctx.send('Invalid value \"%s\"\n' % args[0])
             return
 
-    await ctx.send('%s gets: %s' % (str(varrick.getNick(str(ctx.message.author))), fortuneCookie.eat(numFortunes)))
+    await ctx.send('%s gets: %s' % (str(varrick.get_nick(str(ctx.message.author))), fortuneCookie.eat(numFortunes)))
+
 
 @commands.command(pass_context=True)
 async def discard(ctx, *args):
-    await ctx.send(savageWorlds.discard(get_name(ctx),args))
+    await ctx.send(savageWorlds.discard(get_name(ctx), args))
+
 
 @commands.command(pass_context=True)
 async def tag(ctx, *args):
     await ctx.send(savageWorlds.tag(get_name(ctx), args))
 
+
 @commands.command(pass_context=True)
 async def cleartag(ctx, *args):
     await ctx.send(savageWorlds.clear_tag(get_name(ctx), args))
+
 
 @commands.command(pass_context=True)
 async def planeshift(ctx, *args):
     await ctx.send(savageWorlds.planeshift(get_name(ctx), args))
 
-# Usage command: 
+
+@commands.command(pass_context=True)
+async def nickname(ctx, *args):
+    await ctx.send(varrick.update_nick(ctx, *args))
+
+# Usage command:
 @commands.command()
 async def usage(ctx):
     if current_game == Game.savageWorlds:
         await ctx.send(savageWorlds.usage())
     else:
         await ctx.send(USAGE_MSG)
+
 
 client.add_command(analyze)
 client.add_command(cardsleft)
@@ -436,10 +475,11 @@ client.add_command(countdown)
 client.add_command(discard)
 client.add_command(draw)
 client.add_command(hand)
+client.add_command(nickname)
 client.add_command(planeshift)
 client.add_command(r)
 client.add_command(roll)
-client.add_command(setgame)
+client.add_command(set_game)
 client.add_command(shuffle)
 client.add_command(tag)
 client.add_command(usage)
